14d3b2e32aff6c3a4fa45e34dfb79f911422b991
SchemataCode <101328991+SchemataCode@users.noreply.github.com>
2022-03-21 16:38:00 +0000

    Added code to generate XSDs and then validate all XMLs for the schemas in the examples folder

--- run_schemata.py
+++ run_schemata.py
@@ -1,15 +1,16 @@
 import logging 
-import schemata 
-import glob 
 import argparse 
-from lxml.etree import parse, XMLSchema 
+import glob 
+from schemata.parser import *
+from schemata.exporters import * 
 import json 
+from lxml.etree import parse, XMLSchema, XSLT, tostring
 
 
 def generate():
-    parser = schemata.Parser()
+    parser = Parser()
 
-    fps = glob.glob("schemata_test_files/*.schema")
+    fps = glob.glob("examples/*.schema")
 
     for fp in fps:
         logging.info("Generating an XSD file from {}.".format(fp))
@@ -19,10 +20,52 @@ def generate():
         with open(fp[:-7] + ".json", "w") as fo:    
             json.dump(schema.toJSON(), fo , indent=4)
 
-        schemata.exportSchemaAsXSD(schema, "vTest", fp[:-7] + ".xsd")
+        exportSchemaAsXSD(schema, "vTest", fp[:-7] + ".xsd")
+
+
+def validate():
+
+    fps1 = glob.glob("examples/*.xsd")
+
+    for fp1 in fps1:
+        fp2 = fp1[:-4] + ".xml"
+
+        xsdDocument = parse(fp1)
+        schema = XMLSchema(xsdDocument)
+
+        logging.info("Checking that all valid examples pass when validated against the XSD file.")
+
+        fps2 = [fp2]
+
+        for fp3 in fps2:
+            xmlDocument = parse(fp3)
+            isValid = schema.validate(xmlDocument)
+
+            if isValid == True:
+                logging.info(" - {} passes.".format(fp3))
+            else:
+                logging.info(" - {} does not pass.".format(fp3))
+
+                schema.assertValid(xmlDocument)
+
+        logging.info("Checking that all invalid examples fail when validated against the XSD file.")
+
+        fps3 = []
+
+        for fp4 in fps3:
+            xmlDocument = parse(fp4)
+            isValid = schema.validate(xmlDocument)
+
+            if isValid == False:
+                logging.info(" - {} fails.".format(fp4))
+            else:
+                logging.info(" - {} does not fail.".format(fp4))
+
+                raise Exception("{} should fail validation, but doesn't.".format(fp4))
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
     generate()
+    validate()
