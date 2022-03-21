b284426d7e7f348fb7c6eb28b2d479e5b59a223f
SchemataCode <101328991+SchemataCode@users.noreply.github.com>
2022-03-21 18:09:29 +0000

    Changed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="schemata",
+    name="schematacode",
     version="1.0.0",
     author="B. T. Milnes",
     description="Schemata is a file format that makes writing XML and JSON schemas easier.",
